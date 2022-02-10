96b6eb095695498722d8aa3a3c228c334291260d
siavashsajady <hubgitpro@gmail.com>
2022-02-10 11:37:34 +0100

    implement navigation bar & create Header and Layout components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
+import { Layout } from '../components'
 import 'tailwindcss/tailwind.css'
 import '../styles/globals.scss'
 
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
