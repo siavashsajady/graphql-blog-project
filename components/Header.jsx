0778197b009e252258483feb101d711f24874486
siavashsajady <hubgitpro@gmail.com>
2022-02-12 13:51:15 +0100

    fetch data from graphcms

--- components/Header.jsx
+++ components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
       <div className="inline-block w-full border-b border-blue-400 py-8">
         <div className="block md:float-left">
           <Link href="/">
-            <span className="cursor-pointer text-4xl font-bold text-white">
+            <span className="cursor-pointer text-4xl font-bold text-black">
               GraphBlog
             </span>
           </Link>
@@ -20,7 +20,7 @@ const Header = () => {
         <div className="hidden md:float-left md:contents">
           {categories.map((category) => (
             <Link key={category.slug} href={`/category/${category.slug}`}>
-              <span className="mt-2 ml-4 cursor-pointer align-middle font-semibold text-white md:float-right">
+              <span className="mt-2 ml-4 cursor-pointer align-middle font-semibold text-black md:float-right">
                 {category.name}
               </span>
             </Link>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,8 @@
 import Head from 'next/head'
 import { PostCard, PostWidget, Categories } from '../components'
+import { getPosts } from '../services'
 
-const posts = [
-  {
-    title: 'React Testing',
-    excerpt: 'Learn React Testing',
-  },
-  {
-    title: 'React with Tailwind',
-    excerpt: 'Learn Reactwith Tailwind',
-  },
-]
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto mb-8 px-10">
       <Head>
@@ -21,7 +12,7 @@ export default function Home() {
       <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
         <div className="col-span-1 lg:col-span-8">
           {posts.map((post, index) => (
-            <PostCard post={post} key={post.title} />
+            <PostCard post={post.node} key={post.title} />
           ))}
         </div>
         <div className="col-span-1 lg:col-span-4">
@@ -33,4 +24,12 @@ export default function Home() {
       </div>
     </div>
   )
+}
+
+// Fetch data at build time
+export async function getStaticProps() {
+  const posts = (await getPosts()) || []
+  return {
+    props: { posts },
+  }
 }
