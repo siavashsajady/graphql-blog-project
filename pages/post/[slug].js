331a6b52e7b0001754f075b89c27a59ecff2def8
siavashsajady <hubgitpro@gmail.com>
2022-02-13 10:20:28 +0100

    bug fixing

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -9,19 +9,22 @@ import {
   CommentsForm,
 } from '../../components'
 
-const PostDetails = () => {
+const PostDetails = ({ post }) => {
   return (
     <div className="container mx-auto mb-8 px-10">
       <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
         <div className="col-span-1 lg:col-span-8">
-          <PostDetail />
-          <Author />
-          <CommentsForm />
-          <Comments />
+          <PostDetail post={post} />
+          <Author author={post.author} />
+          <CommentsForm slug={post.slug} />
+          <Comments slug={post.slug} />
         </div>
         <div className="col-span-1 lg:col-span-4">
           <div className="relative top-8 lg:sticky">
-            <PostWidget />
+            <PostWidget
+              slug={post.slug}
+              categories={post.categories.map((category) => category.slug)}
+            />
             <Categories />
           </div>
         </div>
@@ -31,3 +34,21 @@ const PostDetails = () => {
 }
 
 export default PostDetails
+
+// Fetch data at build time
+export async function getStaticProps({ params }) {
+  const data = await getPostDetails(params.slug)
+  return {
+    props: {
+      post: data,
+    },
+  }
+}
+
+export async function getStaticPaths() {
+  const posts = await getPosts()
+  return {
+    paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
+    fallback: false,
+  }
+}
